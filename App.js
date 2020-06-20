1c9e3685574b74830ec256aba784bb6c1cf6688d
oluwadamilola <dammyolukoju@gmail.com>
2020-06-20 20:51:32 +0100

    (implement-productScreen)

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Container } from 'native-base';
-import Login from './src/Screens/Login'
-import Register from './src/Screens/Register';
-
+import React from "react";
+import { Container } from "native-base";
+import Login from "./src/Screens/Login";
+import Register from "./src/Screens/Register";
+import Products from "./src/Screens/Products";
 
 function App() {
   return (
     <Container>
-      <Register />
+      {/*<Register />*/}
       {/* <Login /> */}
+      <Products />
     </Container>
-  )
+  );
 }
 
 export default App;
